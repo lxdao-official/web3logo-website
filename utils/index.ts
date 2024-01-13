f448a6bf9d4e33487fc89196936a5203e16e3980
wwei-github <418047394@qq.com>
2024-01-13 09:09:01 +0800

    feat: revert 4everland

--- utils/index.ts
+++ utils/index.ts
@@ -20,6 +20,7 @@ export function debounce(func: (...args: any[]) => void, delay: number) {
 }
 
 export function getImg3DidStrFromUrl(url: string) {
+  return url
   if (!url) return url
   const pattern = new RegExp(`\\b[a-zA-Z0-9]{59}\\b`, 'g')
   const matches = url.match(pattern)
